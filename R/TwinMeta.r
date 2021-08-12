ae1308bde436c5dc56c3d4d242decb745d5422fa
Andrey Shabalin <andrey.shabalin@gmail.com>
2021-08-12 16:47:41 -0600

    Added TwinMeta_simulate()

--- R/TwinMeta.r
+++ R/TwinMeta.r
@@ -174,6 +174,192 @@ library(MatrixEQTL)
 }
 
 
+TwinMeta_simulate = function(Nm, Nd, Ns, Ngene, Nsnps, Ncvrt){
+
+    # Default parameters
+    if(FALSE){
+        Nm = 1000
+        Nd = 2000
+        Ns = 3000
+        
+        Ngene = 10000
+        Nsnps = 100000
+        Ncvrt = 10
+    }
+    
+    # Checks
+    {
+        stopifnot(length(Nm) == 1);
+        stopifnot(is.numeric(Nm));
+        stopifnot(is.finite(Nm));
+        stopifnot(Nm >= 0);
+        stopifnot(Nm == as.integer(Nm))
+        
+        stopifnot(length(Nd) == 1);
+        stopifnot(is.numeric(Nd));
+        stopifnot(is.finite(Nd));
+        stopifnot(Nd >= 0);
+        stopifnot(Nd == as.integer(Nd))
+
+        stopifnot(length(Ns) == 1);
+        stopifnot(is.numeric(Ns));
+        stopifnot(is.finite(Ns));
+        stopifnot(Ns >= 0);
+        stopifnot(Ns == as.integer(Ns))
+        
+        stopifnot(length(Ngene) == 1);
+        stopifnot(is.numeric(Ngene));
+        stopifnot(is.finite(Ngene));
+        stopifnot(Ngene >= 1);
+        stopifnot(Ngene == as.integer(Ngene))
+        
+        stopifnot(length(Nsnps) == 1);
+        stopifnot(is.numeric(Nsnps));
+        stopifnot(is.finite(Nsnps));
+        stopifnot(Nsnps >= 1);
+        stopifnot(Nsnps == as.integer(Nsnps))
+        
+        stopifnot(length(Ncvrt) == 1);
+        stopifnot(is.numeric(Ncvrt));
+        stopifnot(is.finite(Ncvrt));
+        stopifnot(Ncvrt >= 0);
+        stopifnot(Ncvrt == as.integer(Ncvrt))
+        
+        stopifnot(Nm + Nd + Ns >= 1);
+    }
+    
+    # Make integers
+    {
+        Nm = as.integer(Nm);
+        Nd = as.integer(Nd);
+        Ns = as.integer(Ns);
+        Ngene = as.integer(Ngene);
+        Nsnps = as.integer(Nsnps);
+        Ncvrt = as.integer(Ncvrt);
+    }
+    
+    # Total number of samples
+    N = 2L * Nm + 2L * Nd + Ns;
+    
+    # Generate set ids
+    {
+        idsMZ1 = seq_len(Nm)
+        idsMZ2 = seq_len(Nm) + Nm;
+        idsDZ1 = seq_len(Nd) + 2*Nm;
+        idsDZ2 = seq_len(Nd) + 2*Nm + Nd;
+        idsS   = seq_len(Ns) + 2*Nm + 2*Nd;
+        
+        # stopifnot(c(idsMZ1,idsMZ2,idsDZ1,idsDZ2,idsS) == seq_len(N));
+        # stopifnot(tail(idsS,1) == N)
+    } # idsMZ1, idsMZ2, idsDZ1, idsDZ2, idsS  
+    
+    # Generate Gene expression
+    {
+        message("Gene: Start generating gene expression")
+        message("Gene: Generating ACE parameters for each gene");        
+        a = 3 + 3 * runif(Ngene);
+        c = 4 + 4 * runif(Ngene);
+        e = 5 + 4 * runif(Ngene);
+        
+        # Genotype effect component
+        message("Gene: Generating A component")
+        A = matrix(0, Ngene, N);
+        A[,-idsMZ2] = rnorm(Ngene*(N-Nm))*a;
+        
+        A[,idsMZ2] = A[,idsMZ1];
+        A[,idsDZ2] = A[,idsDZ1] * 0.5 + sqrt(0.75) * A[,idsDZ2];
+
+        # cor(c(A[,idsDZ1]), c(A[,idsDZ2]))
+        # cor(c(A[,idsMZ1]), c(A[,idsMZ2]))
+        
+        message("Gene: Generating C component")
+        C = matrix(0, Ngene, N);
+        C[,-c(idsMZ2,idsDZ2)] = rnorm(Ngene*(N-Nm-Nd))*c;
+        
+        C[,idsMZ2] = C[,idsMZ1];
+        C[,idsDZ2] = C[,idsDZ1];
+        
+        # cor(c(C[,idsDZ1]), c(C[,idsDZ2]))
+        # cor(c(C[,idsMZ1]), c(C[,idsMZ2]))
+        
+        message("Gene: Generating E component");
+        E = rnorm(Ngene*N)*e;
+        dim(E) = c(Ngene, N);
+        
+        message("Gene: Generating gene expression as A+C+E")
+        gene = A + C + E;
+        colnames(gene) = sprintf("Sample_%06d", seq_len(N));
+        rownames(gene) = sprintf("Gene_%06d", seq_len(Ngene));
+        
+        rm(A, C, E);
+        rm(a, c, e);
+        
+        message("Gene: Done generating gene expression")
+    }
+    
+    # Generate genotypes
+    {
+        message("SNPs: Start generating genotypes")
+        message("SNPs: Generating MAF for each SNP") 
+        maf = runif(Nsnps)^2*0.49+0.01;
+        # A1 = rbinom(Nsnps*N, size = 1, prob = maf);
+        
+        message("SNPs: Generating Allele 1 with given MAF");
+        A1 = matrix(NA_integer_, Nsnps, N);
+        A1[,-c(idsMZ2,idsDZ2)] = as.integer(runif(Nsnps*(N - Nm - Nd)) < maf);
+
+        message("SNPs: Generating Allele 2 with given MAF");
+        A2 = matrix(NA_integer_, Nsnps, N);
+        A2[,-idsMZ2] = as.integer(runif(Nsnps*(N - Nm)) < maf);
+        
+        # system.time({ A1 = rbinom(Nsnps*N, size = 1, prob = maf); dim(A1) = c(Nsnps, N); })
+        # system.time({ A1 = as.integer(runif(Nsnps*N) < maf); dim(A1) = c(Nsnps, N); })
+        
+        # A1[1:10,1:10]
+        # as.matrix(maf[1:10])
+        
+        message("SNPs: Generating SNP matrix");
+        snps = matrix(-1L, nrow = Nsnps, ncol = N);
+        
+        message("SNPs: Filling SNP matrix for MZ twins");
+        snps[,idsMZ1] = A1[,idsMZ1] + A2[,idsMZ1];
+        snps[,idsMZ2] = snps[,idsMZ1];
+        
+        message("SNPs: Filling SNP matrix for DZ twins");
+        snps[,idsDZ1] = A1[,idsDZ1] + A2[,idsDZ1];
+        snps[,idsDZ2] = A1[,idsDZ1] + A2[,idsDZ2];
+        
+        message("SNPs: Filling SNP matrix for Singletons");
+        snps[,idsS  ] = A1[,idsS  ] + A2[,idsS  ];
+        
+        # min(snps)
+        # 
+        # cor(c(snps[,idsMZ1]),c(snps[,idsMZ2]))
+        # cor(c(snps[,idsDZ1]),c(snps[,idsDZ2]))
+        # 
+        # cor(c(snps[,c(idsMZ1,idsDZ1)]),c(snps[,idsS]))
+        
+        colnames(snps) = colnames(gene);
+        rownames(snps) = sprintf("Snp_%06d", seq_len(Nsnps));
+        
+        rm(maf);
+        rm(A1, A2);
+        
+        message("SNPs: Done generating genotypes")
+    }
+    
+    # Covariates
+    {
+        message("Cvrt: Start generating covariates") 
+        cvrt = rnorm(Ncvrt*N);
+        dim(cvrt) = c(Ncvrt, N);
+        colnames(cvrt) = colnames(gene);
+        rownames(cvrt) = sprintf("Covt_%06d", seq_len(Ncvrt));
+        message("Cvrt: Done generating covariates") 
+    }
+    return(list(gene = gene, snps = snps, cvrt = cvrt));
+}
+
 TwinMeta_testAll = function(
 	snps1, gene1, cvrt1,
 	snps2, gene2, cvrt2,
